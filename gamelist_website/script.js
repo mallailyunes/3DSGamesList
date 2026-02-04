ca88b90eb495901cbeba64e932339e298eecd553
mallailyunes <yunesm3@nycstudents.net>
2026-02-04 09:26:55 -0500

    Added code to allow the user to input something.

--- gamelist_website/script.js
+++ gamelist_website/script.js
@@ -4,7 +4,11 @@ let data;
 let grid = document.querySelector(".grid-container");
 let form = document.querySelector(".form");
 
-var xhttp = new XMLHttpRequest();
+var newObj = {
+    title: titleInput.value,
+    publisher: pubInput.value,
+    releaseDate: dateInput.value
+  };
 
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -33,19 +37,68 @@ xhttp.onreadystatechange = function() {
     }
 };
 
+// LOAD DATA (localStorage first, otherwise XHR)
+if (localStorage.getItem("datalist")) {
+    data = JSON.parse(localStorage.getItem("datalist"));
+    console.log("Loaded from localStorage");
+    if (grid) {
+      makeCards();
+    }
+  } else {
+    var xhttp = new XMLHttpRequest();
+  
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        data = JSON.parse(this.responseText);
+        console.log("Loaded from gameData.json");
+  
+        localStorage.setItem("datalist", JSON.stringify(data));
+        console.log("Saved starter data to localStorage");
+  
+        if (grid) {
+          makeCards();
+        }
+      }
+    };
+
+    xhttp.open("GET", "gameData.json", true);
+    xhttp.send();
+}
+
+// RENDER CARDS
+function makeCards() {
+    grid.innerHTML = "";
+  
+    data.forEach(function (game) {
+      let card = document.createElement("div");
+      card.classList.add("card");
+  
+      let textData =
+        "<div class='game-title'>" + game.title + "</div>" +
+        "<div>Publisher: " + game.publisher + "</div>" +
+        "<div>Release Date: " + game.releaseDate + "</div>";
+
+        card.innerHTML = textData;
+        grid.appendChild(card);
+      });
+    
+      console.log("cards refreshed");
+    }    
+  
+
 form.addEventListener("submit",function(e){
     e.preventDefault();
     let title = titleInput.value;
+    let releaseDate = releaseDateInput.value;
     let developer = developerInput.value;
     let publisher = devInput.value;
-    let releaseDate = releaseDateInput.value;
     let imgSrc = imgInput.value;
     let newObj = {
         "id":getNextId(),
         "title": title,
+        "releaseDate":releaseDate,
         "developer":developer,
         "publisher": publisher,
-        "releaseDate":releaseDate,
         "imgSrc":imgSrc };
     submitData(newObj);
     form.reset();
